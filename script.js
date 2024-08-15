090c6a271786faba7329ee566bffa951d81ceb07
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-08-15 13:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1=''||s2=''){
+		console.log("''");
+	}
+	for(i=0;i<s1.length;i++){
+		console.log(s1[0]);
+	}
+	for(i=0;i<s2.length;i++){
+		console.log(s2[0]);
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
